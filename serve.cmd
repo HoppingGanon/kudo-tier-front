60df2a5c2ff43ccdb162a6adeb8684cabee12b76
HoppingGanon <hoppingganonapp@gmail.com>
2023-02-18 18:46:39 +0900

    [Add] 本番環境用ビルドに向けた調整

--- serve.cmd
+++
@@ -1,4 +0,0 @@
-cd %~dp0
-title front
-npm run serve
-pause
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PUBLIC_PATH ? process.env.VUE_APP_PUBLIC_PATH : '/',
 
   devServer: {
-    port: 8200
+    port: 8200,
     // host: 'localhost'
+    allowedHosts: ['localhost', 'tier-reviews.mydns.jp'],
+    compress: true
   },
 
   pluginOptions: {
@@ -18,5 +21,14 @@ module.exports = defineConfig({
       entry: 'src/main.ts',
       title: 'TierReviews'
     }
+  },
+  chainWebpack: config => {
+    config.plugin('VuetifyLoaderPlugin').tap(args => [{
+      match (originalTag, { kebabTag, camelTag }) {
+        if (kebabTag.startsWith('core-')) {
+          return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+        }
+      }
+    }])
   }
 })
